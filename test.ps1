410e13142a7a2999516bb03f40ff497971c939a1
kaushal1401 <42976577+kaushal1401@users.noreply.github.com>
2019-11-15 20:40:23 +0530

    test

--- test.ps1
+++ test.ps1
@@ -1,6 +1,2 @@
-# Create Temp Directory
-if (-not(Test-Path -Path 'C:\test\hello\new'))
-{
-    New-Item -Path 'C:\test\hello\new' -ItemType directory
-}
+
 invoke-item "C:\Program Files\Notepad++\notepad++.exe"
